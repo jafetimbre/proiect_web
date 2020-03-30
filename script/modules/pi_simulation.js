e09f148ef6b1e1d03f5c7294141204c8d3e5bbdf
jafetimbre <jafet.imbre@gmail.com>
2020-03-30 19:34:18 +0300

    adding motion
    added random walk motion for every dot

--- script/modules/pi_simulation.js
+++ script/modules/pi_simulation.js
@@ -1,83 +1,109 @@
-export default function pi_simulation(p) {
-  let box1;
-  let box2;
-  let countDiv;
+export default class pi_simulation {
+  
+  constructor(btns) {
+    this.btns = btns;
+    this.canvas = null;
+    this.running = false;
+  }
 
-  const timeStep = 5000; // 50000000
+  scetch = p => {
+    var box1;
+    var box2;
+    var countDiv;
+  
+    var timeStep = 5000; // 50000000
+  
+    var digits = 5;
+    var value = Math.pow(100, digits - 1);
+  
+    var count = 0;
 
-  const digits = 5;
-  let value = Math.pow(100, digits - 1);
+    this.btns[0].addEventListener("click", () => {
+      p.loop();
+      this.running = true;
+    });
+    this.btns[1].addEventListener("click", () => {
+      p.noLoop();
+      this.running = false;
+    });
+    this.btns[2].addEventListener("click", () => {
+      reset();
+    });
 
-  let count = 0;
-
-  p.setup = () => {
-    p.createCanvas(600, 480);
-    p.background(200);
-    box1 = new Box(200, 50, 1, 0, 0);
-    box2 = new Box(400, 100, value, -2 / timeStep, 50);
-    countDiv = p.createDiv(count);
-    countDiv.style('font-size', '30pt');
-  };
-
-  p.draw = () => {
-    p.background(200);
-    for (let i = 0; i < timeStep; ++i) {
-
-      if (box1.collide(box2)) {
-        const v1 = box1.bounce(box2);
-        const v2 = box2.bounce(box1);
-        box1.v = v1;
-        box2.v = v2;
-        count++;
+    p.setup = () => {
+      p.createCanvas(600, 480);
+      p.background(20);
+      box1 = new Box(200, 50, 1, 0, 0);
+      box2 = new Box(400, 100, value, -2 / timeStep, 50);
+      countDiv = p.createDiv(count);
+      countDiv.style('font-size', '30pt');
+      p.noLoop();
+    };
+  
+    p.draw = () => {
+      p.background(50);
+      for (let i = 0; i < timeStep; ++i) {
+  
+        if (box1.collide(box2)) {
+          const v1 = box1.bounce(box2);
+          const v2 = box2.bounce(box1);
+          box1.v = v1;
+          box2.v = v2;
+          count++;
+        }
+        if (box1.hitWall()) {
+          box1.reverse();
+          count++;
+        }
+        box1.update();
+        box2.update();
       }
-      if (box1.hitWall()) {
-        box1.reverse();
-        count++;
-      }
-      box1.update();
-      box2.update();
-    }
-    box1.show();
-    box2.show();
-    countDiv.html(count);
-  };
+      box1.show();
+      box2.show();
+      countDiv.html(count);
+    };
 
-  class Box {
-    constructor(x, w, m, v, xC) {
-      this.x = x;
-      this.y = p.height - w - 1; // -1 for the border
-      this.w = w;
-      this.v = v;
-      this.m = m;
-      this.xConst = xC;
-    }
-  
-    hitWall() {
-      return (this.x <= 0);
-    }
-  
-    reverse() {
-      this.v *= -1;
-    }
-  
-    collide(obj) {
-      return !(this.x + this.w < obj.x || this.x > obj.x + obj.w);
-    }
-  
-    bounce(obj) {
-      let sumM = this.m + obj.m;
-      let nV = (this.m - obj.m) / sumM * this.v + (2 * obj.m / sumM) * obj.v;
-      return nV;
-    }
-  
-    update() {
-      this.x += this.v;
-    }
-  
-    show() {
-      const x = p.constrain(this.x, this.xConst, p.width + 1);
-      p.fill(86, 132, 206);
-      p.rect(x, this.y, this.w, this.w);
-    }
-  };
+    var reset = () => {
+      p.loop();
+      count = 0;
+      box1 = null;
+      box2 = null;
+      box1 = new Box(200, 50, 1, 0, 0);
+      box2 = new Box(400, 100, value, -2 / timeStep, 50);
+      p.noLoop();
+    };
+
+    class Box {
+      constructor(x, w, m, v, xC) {
+        this.x = x;
+        this.y = p.height - w - 1; // -1 for the border
+        this.w = w;
+        this.v = v;
+        this.m = m;
+        this.xConst = xC;
+      }
+      hitWall() {
+        return (this.x <= 0);
+      }
+      reverse() {
+        this.v *= -1;
+      }
+      collide(obj) {
+        return !(this.x + this.w < obj.x || this.x > obj.x + obj.w);
+      }
+      bounce(obj) {
+        let sumM = this.m + obj.m;
+        let nV = (this.m - obj.m) / sumM * this.v + (2 * obj.m / sumM) * obj.v;
+        return nV;
+      }
+      update() {
+        this.x += this.v;
+      }
+      show() {
+        const x = p.constrain(this.x, this.xConst, p.width + 1);
+        p.fill(86, 132, 206);
+        p.rect(x, this.y, this.w, this.w);
+      }
+    };
+  }
 };
--- script/page_4.js
+++ script/page_4.js
@@ -1,4 +1,15 @@
 import pi_simulation from './modules/pi_simulation.js'
+import test from './modules/test.js'
 
-var myp5 = new p5(pi_simulation, 'canvas_2');
 
+var myp5 = new p5(new test([
+    document.getElementById("start-c1"),
+    document.getElementById("stop-c1"),
+    document.getElementById("reset-c1")
+]).scetch, 'canvas_1');
+
+var myp5 = new p5(new pi_simulation([
+    document.getElementById("start-c2"),
+    document.getElementById("stop-c2"),
+    document.getElementById("reset-c2")
+]).scetch, 'canvas_2');
