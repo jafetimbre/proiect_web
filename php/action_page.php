277342f7c8742f5741f0fe88ffc303643f66b2be
jafetimbre <jafet.imbre@gmail.com>
2020-04-21 16:29:08 +0300

    Revert "php"
    This reverts commit b7164aa56e970792584c6f6fc8196588ddc83c30.

--- php/action_page.php
+++
@@ -1,3 +0,0 @@
-<?php
-    echo '<i>Bine ati venit '.$_POST["nume"].' '.$_POST["p_nume"].'</i>';
-?>
--- php/sin_page.php
+++
@@ -1,8 +0,0 @@
-<html>
-    <body>
-        <?php include("./meniu.php") ?>
-        <div style="padding: 5px; border:1px solid black;margin:10px">
-            <img src="grafic.php">
-        </div>
-    </body>
-</html>
